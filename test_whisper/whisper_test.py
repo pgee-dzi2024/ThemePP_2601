9c12927bbc1e8a540fae17d55f0f66ee5e79e6a0
pgee-dzi2024 <pgee_dzi2024@abv.bg>
2026-02-04 11:34:11 +0200

    чиста инсталация на Django

--- test_whisper/whisper_test.py
+++ test_whisper/whisper_test.py
@@ -1,7 +1,14 @@
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
+os.environ["TORCH_USE_CUDA_DSA"] = "0"
 
 import torch
 print(torch.__version__)
-print(torch.cuda.is_available())  # Трябва да върне False без грешка
+print(torch.cuda.is_available())  # трябва да върне False
+
+import whisper
+model = whisper.load_model("small", device="cpu")
+print("Whisper работи успешно на CPU")
+
+
 
