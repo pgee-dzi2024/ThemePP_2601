66b55bfa3264a08da94c2744c649ec7b6809a4e4
pgee-dzi2024 <pgee_dzi2024@abv.bg>
2026-02-06 11:35:24 +0200

    whisper test и html шаблон

--- test_whisper/whisper_test.py
+++ test_whisper/whisper_test.py
@@ -1,14 +1,22 @@
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
-os.environ["TORCH_USE_CUDA_DSA"] = "0"
-
-import torch
-print(torch.__version__)
-print(torch.cuda.is_available())  # трябва да върне False
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
 import whisper
-model = whisper.load_model("small", device="cpu")
-print("Whisper работи успешно на CPU")
+from pathlib import Path
 
+audio_path = Path(__file__).parent / "audio" / "new.wav"
+
+print("Loading Whisper large model (CPU)...")
+model = whisper.load_model("large", device="cpu")
+
+print("Transcribing audio...")
+
+result = model.transcribe(str(audio_path), language="bg", task="transcribe", beam_size=1, temperature=0.0,
+                          initial_prompt="Това е разговор на български език. Моля, транскрибирай внимателно думите.",
+                          compression_ratio_threshold=2.2, logprob_threshold=-0.6, no_speech_threshold=0.5,
+                          condition_on_previous_text=False, word_timestamps=False, fp16=False)
+
+print("\n--- TRANSCRIPTION ---")
+print(result["text"])
 
 
